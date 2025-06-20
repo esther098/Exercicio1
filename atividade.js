81e5c2915060611aaa02e0ce378363ebeb9c9bb0
Esther <e.orlandeli@aluno.ifsp.edu.br>
2025-06-19 22:41:36 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,10 @@
+function imparPar(){
+numero = parseFloat(document.formulario.numero.value);
+if(numero%2===0){
+    alert("Seu número é par")
+
+}else{
+    alert("seu número é IMPAR, otário.")
+}
+
+}
